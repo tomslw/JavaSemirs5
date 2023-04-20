a987aca3c0069ac26326a7c4913f866d00fcc4e9
Toms Štrāls <s21stratoms@vea.lv>
2023-04-20 15:29:47 +0300

    init

---
+++ src/main/java/com/example/demo/JavaSemminar5AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSemminar5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSemminar5AppApplication.class, args);
+	}
+
+}
