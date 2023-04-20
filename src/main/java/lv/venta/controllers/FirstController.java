2fa412ba77e9099e6509b06dc8a8f42b86c1a784
Toms Štrāls <s21stratoms@vea.lv>
2023-04-20 16:02:22 +0300

    second push

---
+++ src/main/java/lv/venta/controllers/FirstController.java
@@ -0,0 +1,24 @@
+package lv.venta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class FirstController {
+
+	
+	@GetMapping("/hello")
+	public String getHelloFunc() {
+		System.out.println("ay baus");
+		return "hello-page";
+	}
+	
+	@GetMapping("/msg")
+	public String handleMsg(Model model) {
+		model.addAttribute("message", "this is a message");
+		return "msg-page";
+	}
+	
+}
